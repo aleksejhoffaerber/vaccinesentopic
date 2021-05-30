28279afd8592509fcdde133f78b219efb1f174ca
aleksejhoffaerber <aleksejhoffaerber@users.noreply.github.com>
2021-05-30 23:57:28 +0200

    create credential file

---
+++ twitter_credentials.R
@@ -0,0 +1,10 @@
+# account credentials
+
+bearer_token <- "AAAAAAAAAAAAAAAAAAAAANGsQAEAAAAAVqIajpHCsttpQXrLSUhzgoGBMxg%3D1rES3hXHKCfFy6HhGcZGGMiE3b8MFbkyVD00Q3XuKOj0B84DJQ"
+headers <- c(`Authorization` = sprintf('Bearer %s', bearer_token))
+
+api_key <- "Onr9QZ6CGZnE2CzGzzbCxyh61"
+api_secret <- "FGOeYy9zgWdrFF1jRMGfcg28xtbljxY9M6VilpWum8B2aE1Way"
+
+acc_token <- "1394959278528733190-J3JiyE7ZxRvrtzRO1tLLdoLgwUmlvn"
+acc_secret <- "Pv9D5EXIUyxZQf5g03XIY5AwuFgT4cDM439x2nJxFJaw0"
